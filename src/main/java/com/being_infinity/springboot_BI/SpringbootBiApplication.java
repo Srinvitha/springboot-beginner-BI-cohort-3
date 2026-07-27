601c911a17da9147f7355cec481f00150e7c7414
Srinvitha <srinvitha.nutakki@gmail.com>
2026-07-27 22:39:22 +0530

    Springboot initialization

---
+++ src/main/java/com/being_infinity/springboot_BI/SpringbootBiApplication.java
@@ -0,0 +1,13 @@
+package com.being_infinity.springboot_BI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/being_infinity/springboot_BI/SpringbootBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.being_infinity.springboot_BI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
