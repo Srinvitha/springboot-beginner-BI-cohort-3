eb29c242407c6a4367aea28a9c8cc1115e4b4873
Srinvitha <srinvitha.nutakki@gmail.com>
2026-07-28 22:54:22 +0530

    2nd card

--- target/classes/static/Practice/index.js
+++ target/classes/static/Practice/index.js
@@ -1,1 +1,28 @@
-alert("Hello from JS");
+//alert("Hello from JS");
+
+var users = [
+    {
+        "image" : "/images/girl.png",
+        "name" : "John Doe",
+        "gender" : "Male"
+    },
+    {
+        "image" : "/images/boy.png",
+        "name" : "Jane Doe",
+        "gender" : "Female"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1) % users.length;
+    var userImage = document.getElementById("user-image");
+    var userName = document.getElementById("user-name");
+    var userGender = document.getElementById("user-gender");
+
+    userImage.src = users[id].image;
+    userName.innerHTML = users[id].name;
+    userGender.innerHTML = users[id].gender;
+
+}
